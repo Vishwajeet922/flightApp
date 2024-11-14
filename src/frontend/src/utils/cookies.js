ced7c6f072f5fbeffc793309442dc8d8d4a2ba83
Vishwajeet922 <x23319330@student.ncirl.ie>
2024-11-14 16:40:48 +0000

    Cookies fix

--- src/frontend/src/utils/cookies.js
+++ src/frontend/src/utils/cookies.js
@@ -1,15 +1,10 @@
 import Cookies from 'js-cookie';
 
 export const setAuthToken = (token) => {
-  const isProduction = window.location.protocol === 'https:';
-  const domain = window.location.hostname;
-  
-  Cookies.set('jwt', token, {
+  Cookies.set("jwt", token, {
     expires: 1,
-    secure: true,
-    sameSite: 'strict',
-    domain: isProduction ? domain : undefined,
-    path: '/'
+    sameSite: "lax",
+    path: "/",
   });
 };
 
